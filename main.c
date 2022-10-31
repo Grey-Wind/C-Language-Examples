f2d35f53365668222d1815c5b07fad23d460504b
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-10-31 19:47:43 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	
+	
 	system("pause");
 	return 0;
 }
